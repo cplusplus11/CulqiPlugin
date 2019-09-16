22dae8555e7b293195d0517337ccd85c40ef2635
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-09-16 17:10:43 -0500

    Update ToastyPlugin.java

--- src/android/com/luis/ToastyPlugin.java
+++ src/android/com/luis/ToastyPlugin.java
@@ -68,8 +68,8 @@ public class ToastyPlugin extends CordovaPlugin {
 	Intent intent = new Intent(context, PayActivity.class);
 	intent.putExtra(Constants.PAYME_BUNDLE,bundle);        
 	    
-	this.cordova.startActivityForResult((CordovaPlugin) this,intent, Constants.REQUEST_CODE_PAYME);  
-	    
+	//this.cordova.startActivityForResult((CordovaPlugin) this,intent, Constants.REQUEST_CODE_PAYME);  
+	 this.cordova.startActivityForResult(this,intent, Constants.REQUEST_CODE_PAYME);     
      	Toast toast1 = Toast.makeText(cordova.getActivity(), "alberto 3",Toast.LENGTH_SHORT);
 	toast1.show(); 
      	/**/
