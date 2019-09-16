34fd6fd4d6a4743196555929bf5faa12a27d41bc
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-09-16 11:58:25 -0500

    Update ToastyPlugin.java

--- src/android/com/luis/ToastyPlugin.java
+++ src/android/com/luis/ToastyPlugin.java
@@ -49,7 +49,7 @@ public class ToastyPlugin extends CordovaPlugin {
         //intent.putExtra(Constants.PAYME_BUNDLE,bundle);        
         //this.cordova.startActivityForResult((CordovaPlugin) this,intent, "Hola");	
 	    
-	Bundle bundle = new Bundle();
+	/*Bundle bundle = new Bundle();
         bundle.putString(Constants.EXTRA_MERCHANT_ENVDOMAIN, URL);
         bundle.putParcelable(Constants.EXTRA_MERCHANT_OPERATION, merchantOperation);
         bundle.putBoolean(Constants.EXTRA_ENABLED_MPOS, false);    
@@ -57,7 +57,7 @@ public class ToastyPlugin extends CordovaPlugin {
 	Intent intent = new Intent(context, PayActivity.class);
 	intent.putExtra(Constants.PAYME_BUNDLE,bundle);        
 	    
-	this.cordova.startActivityForResult((CordovaPlugin) this,intent, Constants.REQUEST_CODE_PAYME);  
+	this.cordova.startActivityForResult((CordovaPlugin) this,intent, Constants.REQUEST_CODE_PAYME);  */
 	    
      	//Toast toast1 = Toast.makeText(cordova.getActivity(), message,Toast.LENGTH_SHORT);
 	//toast1.show(); 
