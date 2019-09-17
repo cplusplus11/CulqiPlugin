c0ec8d6c397cbad887be7b672de630ff3b7cf757
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-09-17 10:36:44 -0500

    Update toastyplugin.js

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -11,8 +11,8 @@ function ToastyPlugin() {}
 // recibir los datos
 //ToastyPlugin.prototype.show = function(message, successCallback, errorCallback) {
 ToastyPlugin.prototype.show = function(arg0, successCallback, errorCallback) {
-  var options = {};
-  options.message = message;
+  //var options = {};
+  //options.message = message;
   //cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
   cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [arg0]);
 }
