a6f4a14e8825f30c4e821f61f742bd40b561abc4
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-09-17 10:30:31 -0500

    Update toastyplugin.js

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -9,10 +9,12 @@ exports.show = function (arg0, success, error) {
 function ToastyPlugin() {}
 
 // recibir los datos
-ToastyPlugin.prototype.show = function(message, successCallback, errorCallback) {
+//ToastyPlugin.prototype.show = function(message, successCallback, errorCallback) {
+ToastyPlugin.prototype.show = function(arg0, successCallback, errorCallback) {
   var options = {};
   options.message = message;
-  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
+  //cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
+  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [arg0]);
 }
 
  // Instalar la variable en el window
